0a43047f3310177b792136e1c45f3a390cfbfc3a
Michael Bestas <mkbestas@lineageos.org>
2025-01-01 20:12:21 +0200

    Revert "sdm/hwc: add display indices property for client id init"
    This reverts commit 7082e1e83e569cc44c281d96e7d67df78b264a5d.
    
    Reason for revert: We don't support targets using this, it gets in the
    way of blueprint conversion and it's not present in any other HAL.
    
    Change-Id: If258ae899292a55a8272625f88b53db1e2896d44

--- sdm/libs/hwc2/Android.mk
+++ sdm/libs/hwc2/Android.mk
@@ -38,10 +38,6 @@ LOCAL_SHARED_LIBRARIES        := libsdmcore libqservice libbinder libhardware li
                                  vendor.display.config@1.9 \
                                  vendor.display.config@1.10
 
-ifeq ($(TARGET_BOARD_AUTO), true)
-LOCAL_CFLAGS                  += -DCONFIG_BASEID_FROM_PROP
-endif
-
 ifeq ($(TARGET_USES_FOD_ZPOS), true)
 LOCAL_CFLAGS                  += -DFOD_ZPOS
 endif
